dee1d275ca1590745fd777ef50dc050708c633d4
rinkuberde <rinkuberde@gmail.com>
2023-02-20 21:48:53 +0530

    [Rinku] - Able to add new Contact

---
+++ AddressBook/ContactView.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    class ContactView
+    {
+        public static List<Contacts> contactsList = new List<Contacts>();
+
+        public void ContactViewMethod()
+        {
+            Contacts Person1 = new Contacts
+            {
+                FirstName = "Rinku",
+                LastName = "Berde",
+                Address = "SR nagar",
+                City = "Mysore",
+                State = "Karnataka",
+                ZipCode = 110096,
+                PhoneNumber = 1234567890,
+                Email = "rinkuberde123@mail.com"
+            };
+            Contacts Person2 = new Contacts
+            {
+                FirstName = "Joseph",
+                LastName = "Joe",
+                Address = "Juhu",
+                City = "Mumbai",
+                State = "Maharastra",
+                ZipCode = 400049,
+                PhoneNumber = 1234567890,
+                Email = "123Joe@mail.com"
+            };
+            Person1.ValidateContactDetails();
+            Person2.ValidateContactDetails();
+
+            //storing contact details to List
+            contactsList.Add(Person1);
+            contactsList.Add(Person2);
+
+        }
+        public List<Contacts> GetLIst()
+        {
+            return contactsList;
+        }
+
+        public void Listview()
+        {
+            try
+            {
+                if (contactsList.Count == 0)
+                    Console.WriteLine("No Contacts to Display");
+                else
+                {
+                    foreach (Contacts i in contactsList)
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Contacts");
+                        Console.WriteLine($"Full Name: {i.FirstName} {i.LastName}");
+                        Console.WriteLine($"Phone Number: {i.PhoneNumber}");
+                        Console.WriteLine($"Email: {i.Email}");
+                        Console.WriteLine($"Address: {i.Address}");
+                        Console.WriteLine($"City:{i.City} ");
+                        Console.WriteLine($"State:{i.State} ");
+                        Console.WriteLine($"ZipCode:{i.ZipCode} ");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+        /// <summary>
+        /// New contact method - ask user to enter all details. using console
+        /// </summary>
+        public void NewContact()
+        {
+            try
+            {
+                Contacts Person3 = new Contacts();
+                Console.WriteLine("Add a new contact.");
+                Console.WriteLine("Enter First Name: ");
+                Person3.FirstName = Console.ReadLine();
+                Console.WriteLine("Enter Last Name: ");
+                Person3.LastName = Console.ReadLine();
+                Console.WriteLine("Enter Address: ");
+                Person3.Address = Console.ReadLine();
+                Console.WriteLine("Enter City: ");
+                Person3.City = Console.ReadLine();
+                Console.WriteLine("Enter State: ");
+                Person3.State = Console.ReadLine();
+                Console.WriteLine("Enter ZipCode: ");
+                Person3.ZipCode = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter Phone Number: ");
+                Person3.PhoneNumber = Convert.ToInt64(Console.ReadLine());
+                Console.WriteLine("Enter Email: ");
+                Person3.Email = Console.ReadLine();
+                //validating contact details
+                Person3.ValidateContactDetails();
+                //adding contact to list
+                contactsList.Add(Person3);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("New contact entry aborted.");
+
+            }
+        }
+    }
+}
+
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -4,30 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address Book");
-            Contacts contacts = new Contacts
-            {
-                FirstName = "Rinku",
-                LastName = "Berde",
-                Address = "SR nagar",
-                City = "Mysore",
-                State = "Karnataka",
-                ZipCode = 583201,
-                PhoneNumber = 1234567890,
-                Email = "rinkuberde@gmail.com"
-            };
-            contacts.ValidateContactDetails();
-            Console.WriteLine("Contact Details: ");
-            Console.WriteLine($"Full Name: {contacts.FirstName + contacts.LastName} ");
-            Console.WriteLine($"Phone Number: {contacts.PhoneNumber} ");
-            Console.WriteLine($"Address: {contacts.Address} ");
-            Console.WriteLine($"Address:{contacts.City} ");
-            Console.WriteLine($"Address:{contacts.State} ");
-            Console.WriteLine($"Address:{contacts.ZipCode} ");
-            Console.WriteLine($"Email: {contacts.Email} ");
-            Console.ReadLine();
+            //Console.ReadLine();
+            ContactView contactView = new ContactView();
+            //hard coded contacts initializing.
+            contactView.ContactViewMethod();
+            //display selection for User.
+            Display display = new Display();
+            display.DisplayChoice();
+            display.Selection();
         }
-      
-    }
-    
+    }    
 }
